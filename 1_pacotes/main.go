793cccbc4468b01811189484a34ca1b11b495bef
lfboeff <116302334+lfboeff@users.noreply.github.com>
2024-03-09 17:01:27 -0300

    pacotes externos

--- 1_pacotes/main.go
+++ 1_pacotes/main.go
@@ -3,9 +3,14 @@ package main
 import (
 	"fmt"
 	aux "my_module/pacote_auxiliar"
+
+	"github.com/badoux/checkmail"
 )
 
 func main() {
 	fmt.Println("Escrevendo do arquivo main")
 	aux.Escrever()
+
+	erro := checkmail.ValidateFormat("devbook@gmail.com")
+	fmt.Println(erro)
 }
