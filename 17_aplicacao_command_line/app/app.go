2d0d3e5f535a45bc91be2fd411da82e945257a0b
lfboeff <116302334+lfboeff@users.noreply.github.com>
2024-03-13 21:48:46 -0300

    aplicacao command line - pt 2

--- 17_aplicacao_command_line/app/app.go
+++ 17_aplicacao_command_line/app/app.go
@@ -1,12 +1,49 @@
 package app
 
-import "github.com/urfave/cli"
+import (
+	"fmt"
+	"log"
+	"net"
+
+	"github.com/urfave/cli"
+)
 
 // Gerar vai retornar a aplicação de CLI pronta para ser executada
 func Gerar() *cli.App {
+
 	app := cli.NewApp()
+
 	app.Name = "Aplicação de Linha de Comando"
-	app.Usage = "Busca IPs e Nomes de servidor na internet"
+
+	app.Usage = "Busca IPs e Nomes de Servidores na internet"
+
+	app.Commands = []cli.Command{
+		{
+			Name:  "ip",
+			Usage: "Busca IPs de endereços na internet",
+			Flags: []cli.Flag{
+				cli.StringFlag{
+					Name:  "host",
+					Value: "devbook.com.br",
+				},
+			},
+			Action: buscarIPs,
+		},
+	}
 
 	return app
+}
+
+func buscarIPs(c *cli.Context) {
+	host := c.String("host")
+	fmt.Println(host)
+
+	ips, err := net.LookupIP(host)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, ip := range ips {
+		fmt.Println(ip)
+	}
 }
